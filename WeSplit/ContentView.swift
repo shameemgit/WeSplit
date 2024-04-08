6b26fd5515e877b9dd546bda24ed5adf9f312a68
Shameem M A <shameem331331@gmail.com>
2024-04-08 14:10:00 +0400

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Shameem M A on 08/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Shameem M A on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
